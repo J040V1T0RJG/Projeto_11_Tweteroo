b80e3c6f122ca44f61bbcdf7451f1fae4d9c1e26
J040V1T0R <joaovitor.jovg73@gmail.com>
2022-06-20 09:28:12 -0300

    requisitos obrigatórios concluido

--- index.js
+++ index.js
@@ -2,10 +2,41 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
-app.use(cors());
+app.use(cors(), express.json());
 
-app.get("/", (req, res) => {
-    res.send("catapimbas meu")
+let user = {username: "", avatar: ""};
+let tweets = [];
+let dataTweets = [];
+
+app.post("/sign-up", (request, response) => {
+    user.username = request.body.username;
+    user.avatar = request.body.avatar;
+    response.send("OK")
 });
+
+app.get("/tweets", (request, response) => {
+    response.send(tweets)
+});
+
+app.post("/tweets", (request, response) => {
+    tweets = [];
+    dataTweets.push({
+            username: user.username,
+            avatar: user.avatar,
+            tweet: request.body.tweet
+    });
+    if (dataTweets.length > 10) {
+        
+        let temporaryArray = []
+        for (let i = 1; i <= 10; i++) {
+            temporaryArray.push(dataTweets[i])
+        }
+        dataTweets = [...temporaryArray];
+    };
+    for (let i = dataTweets.length - 1; i >= 0; i--) {
+        tweets.push(dataTweets[i])
+    };
+    response.send("OK")
+})
 
 app.listen(5000);
